23be88ed96e3893efbd50c42d9a9f3f37452cc7a
schwester34 <yana64604@gmail.com>
2022-06-27 19:47:54 +0500

    add smth

--- src/test/java/tests/TestsForHh.java
+++ src/test/java/tests/TestsForHh.java
@@ -16,7 +16,7 @@ import static io.qameta.allure.Allure.step;
 public class TestsForHh extends TestBase {
     SelenideElement vacancy = $(".HH-MainContent"),
             city = $(".bloko-text"),
-            skill = $(".vacancy-serp-item__info"),//*[@id="a11y-main-content"]
+            skill = $(".vacancy-serp-item__info"),  //*[@id="a11y-main-content"]
             form = $(".bloko-form-item"),
             attribute = $(byAttribute("data-qa", "account-signup-email")),
             ok = $(".signup-submit"),
@@ -25,7 +25,7 @@ public class TestsForHh extends TestBase {
     @DisplayName("Positive tests")
     void hhTest01() {
         step("open the link", () -> {
-            open("https://ekaterinburg.hh.ru/search/vacancy?text=QA+engineer+%D0%B5%D0%BA%D0%B0%D1%82%D0%B5%D1%80%D0%B8%D0%BD%D0%B1%D1%83%D1%80%D0%B3+java&from=suggest_post&salary=&clusters=true&area=3&ored_clusters=true&enable_snippets=true");
+            open("/vacancy?text=QA+engineer+%D0%B5%D0%BA%D0%B0%D1%82%D0%B5%D1%80%D0%B8%D0%BD%D0%B1%D1%83%D1%80%D0%B3+java&from=suggest_post&salary=&clusters=true&area=3&ored_clusters=true&enable_snippets=true");
         });
 
         step("check the correspondence of the job title to the expectation: QA Engineer", () -> {
@@ -51,7 +51,7 @@ public class TestsForHh extends TestBase {
     @DisplayName("Negative test")
     void hhTest02() {
         step("open the vacancies link", () -> {
-            open("https://ekaterinburg.hh.ru/search/vacancy?text=QA+engineer+python&salary=&clusters=true&area=3&ored_clusters=true&enable_snippets=true");
+            open("/vacancy?text=QA+engineer+%D0%B5%D0%BA%D0%B0%D1%82%D0%B5%D1%80%D0%B8%D0%BD%D0%B1%D1%83%D1%80%D0%B3+java&from=suggest_post&salary=&clusters=true&area=3&ored_clusters=true&enable_snippets=true");
         });
         step("checking compliance in key skills Python", () -> {
             skill.scrollTo();
