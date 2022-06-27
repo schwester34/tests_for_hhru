48bde0975c81ae24896c58c4a52ab8e2d9d3a5ba
schwester34 <yana64604@gmail.com>
2022-06-27 20:14:45 +0500

    add smth

--- src/test/java/tests/Tests.java
+++ src/test/java/tests/Tests.java
@@ -13,8 +13,8 @@ import static io.qameta.allure.Allure.step;
 
 
 @Tag("vacancy")
-public class TestsForHh extends TestBase {
-    SelenideElement vacancy = $(".HH-MainContent"),
+public class Tests extends TestBase {
+    SelenideElement vacancy = $(".vacancy-serp__vacancy-title"),
             city = $(".bloko-text"),
             skill = $(".vacancy-serp-item__info"),  //*[@id="a11y-main-content"]
             form = $(".bloko-form-item"),
@@ -25,11 +25,11 @@ public class TestsForHh extends TestBase {
     @DisplayName("Positive tests")
     void hhTest01() {
         step("open the link", () -> {
-            open("/vacancy?text=QA+engineer+%D0%B5%D0%BA%D0%B0%D1%82%D0%B5%D1%80%D0%B8%D0%BD%D0%B1%D1%83%D1%80%D0%B3+java&from=suggest_post&salary=&clusters=true&area=3&ored_clusters=true&enable_snippets=true");
+            open("/vacancy?text=Qa+engineer+java&from=suggest_post&salary=&clusters=true&area=3&ored_clusters=true&enable_snippets=true");
         });
 
-        step("check the correspondence of the job title to the expectation: QA Engineer", () -> {
-            vacancy.shouldHave(text("QA"));
+        step("check the correspondence of the job title to the expectation: Middle QA Engineer", () -> {
+            vacancy.shouldHave(text("Middle QA Engineer"));
         });
 
         step("check the compliance of the city with the vacancy: Екатеринбург", () -> {
@@ -51,7 +51,7 @@ public class TestsForHh extends TestBase {
     @DisplayName("Negative test")
     void hhTest02() {
         step("open the vacancies link", () -> {
-            open("/vacancy?text=QA+engineer+%D0%B5%D0%BA%D0%B0%D1%82%D0%B5%D1%80%D0%B8%D0%BD%D0%B1%D1%83%D1%80%D0%B3+java&from=suggest_post&salary=&clusters=true&area=3&ored_clusters=true&enable_snippets=true");
+            open("/vacancy?text=Qa+engineer+java&from=suggest_post&salary=&clusters=true&area=3&ored_clusters=true&enable_snippets=true");
         });
         step("checking compliance in key skills Python", () -> {
             skill.scrollTo();
